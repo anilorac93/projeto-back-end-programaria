9c1078c1e97b93d7b65e2bad64524f7b31f2c263
anilorac93 <mariotto.carolina@hotmail.com>
2023-05-29 22:48:24 -0300

    Incluindo verbos HTTP

--- mulheres.js
+++ mulheres.js
@@ -1,34 +1,95 @@
 const express = require ('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 const porta = 3333;
 
 const mulheres = [
     {
+        id:'1',
         nome: 'Simara Conceição',
         imagem: 'https://github.com/simaraconceicao.png',
         minibio: 'Desenvolvedora e Instrutora'
     },
     {
+        id:'2',
         nome: 'Iana Chan',
         imagem: 'https://bit.ly/3JCX8qP',
         minibio: 'Fundadora da Programaria'
     },
     {
+        id:'3',
         nome: 'Nina da Hora',
         imagem: 'https://bit.ly/3FKpFaz',
         minibio: 'Hacker antiracista'
     }
 ];
 
+//GET
 function mostraMulheres(request, response) {
-    response.json(mulheres)
+    response.json(mulheres);
 };
+
+// POST
+function criaMulher(request, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+
+    mulheres.push(novaMulher);
+
+    response.json(mulheres);
+};
+
+//PATCH
+function corrigeMulher(request, response) {
+    function encontraMulher(mulher) {
+        if(mulher.id === request.params.id) {
+            return mulher;
+        };
+    };
+    const mulherEncontrada = mulheres.find(encontraMulher);
+
+    if(request.body.nome) {
+        mulherEncontrada.nome = request.body.nome;
+    };
+
+    if(request.body.minibio) {
+        mulherEncontrada.minibio = request.body.minibio;
+    };
+
+    if(request.body.imagem) {
+        mulherEncontrada = request.body.imagem;
+    };
+
+    response.json(mulheres);
+};
+
+//DELETE
+function deletaMulher(request, response) {
+    function todasMenosEla(mulher) {
+        if(mulher.id !== request.params.id) {
+                return mulher;
+        };
+    };
+
+    const mulheresQueFicam = mulheres.filter(todasMenosEla);
+
+    response.json(mulheresQueFicam);
+};
+
+app.use(router.get('/mulheres', mostraMulheres));
+app.use(router.post('/mulheres', criaMulher));
+app.use(router.patch('/mulheres/:id', corrigeMulher));
+app.use(router.delete('/mulheres/:id', deletaMulher));
 
 function mostraPorta() {
     console.log('Servidor criado e rodando na porta ', porta);
 };
 
-app.use(router.get('/mulheres', mostraMulheres));
 app.listen(porta, mostraPorta);
